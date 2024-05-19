df15e55d50c2c1c077b7ddd6676f0934e6f2d336
SoCloseToHeaven <halsofods@gmail.com>
2024-05-19 17:44:13 +0300

    add getting user from db or creating a new one, add basic message handler logic

--- telegram/handler.go
+++ telegram/handler.go
@@ -32,20 +32,54 @@ func NewMessageHandler(db *gorm.DB, bot *tgbotapi.BotAPI) MessageHandler {
 	}
 }
 
+// add logging
+
 func (r *updateHandler) Handle(u tgbotapi.Update) {
-	// TODO: add logic
-
-}
-
-func (r *updateHandler) setUserState(user *model.User, newState model.UserState) error {
-	user.State = newState
-	if err := r.UserRepository.Update(user); err != nil {
-		return err
+	if u.Message == nil {
+		return
 	}
 
-	msg := tgbotapi.NewMessage(user.ChatID, "")
-	msg.ReplyMarkup = StateToKeyboard[newState]
+	user, err := r.getUser(u)
 
-	_, err := r.Bot.Send(msg)
-	return err
+	if err != nil {
+		return
+	}
+
+	reply := tgbotapi.NewMessage(user.ChatID, "privetik")
+	r.setStateKeyboard(user, &reply)
+	r.Bot.Send(reply)
+}
+
+func (r *updateHandler) setStateKeyboard(user *model.User, msg *tgbotapi.MessageConfig) {
+	keyboard := StateToKeyboard[user.State]
+	keyboard.ResizeKeyboard = true  // Адаптивный размер клавиатуры
+	keyboard.OneTimeKeyboard = true // Клавиатура будет доступна повторно
+
+	msg.ReplyMarkup = StateToKeyboard[user.State]
+}
+
+// TODO: Add transactions
+func (r *updateHandler) getUser(u tgbotapi.Update) (*model.User, error) {
+	chatID := u.Message.Chat.ID
+	tgID := u.Message.From.ID
+
+	user, err := r.UserRepository.GetByChatID(chatID)
+
+	if err == nil {
+		return user, nil
+	}
+
+	user = &model.User{
+		ChatID:     chatID,
+		TelegramID: tgID,
+		State:      model.Main,
+	}
+
+	err = r.UserRepository.Create(user)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
