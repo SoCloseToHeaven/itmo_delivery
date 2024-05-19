207540fd509b1f6bd3fc352233f38d5c686205dc
SoCloseToHeaven <halsofods@gmail.com>
2024-05-19 19:13:38 +0300

    add types and UnknownAction to msg.go

--- telegram/msg.go
+++ telegram/msg.go
@@ -1,6 +1,7 @@
 package telegram
 
 const (
-	StartMsg = "Добро пожаловать в сервис ITMO.DELIVERY"
-	ErrorMsg = "Упс... Что-то пошло не так"
+	StartMsg      string = "Добро пожаловать в сервис ITMO.DELIVERY"
+	ErrorMsg      string = "Упс... Что-то пошло не так"
+	UnknownAction string = "Неизвестное действие!"
 )
