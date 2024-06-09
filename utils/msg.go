37768f206903fc3f8568e5812b6a45b2c6c9cb92
SoCloseToHeaven <halsofods@gmail.com>
2024-06-09 12:19:38 +0300

    add more messages

--- utils/msg.go
+++ utils/msg.go
@@ -3,20 +3,25 @@ package utils
 import "itmo_delivery/model"
 
 const (
-	StartMsg                     string = "Добро пожаловать в сервис ITMO.DELIVERY"
-	ErrorMsg                     string = "Упс... Что-то пошло не так"
-	UnknownAction                string = "Неизвестное действие!"
-	SelectBuilding               string = "Выберите нужное здание в меню."
-	MyOrders                     string = "Мои последние заказы"
-	ActiveOrders                 string = "Активные заказы:"
-	OrderConfirmMessageFormatted string = "Вы подтверждаете заказ? \n%s"
-	InputDescription             string = "Введите описание заказа:"
-	Instruction                  string = "Инструкция"   // TODO: Нормальный текст инструкции
-	Feedback                     string = "Фидбек"       // TODO: Нормальный текст фидбека
-	AboutBot                     string = "О боте"       // TODO: Нормальный текст о боте
-	MainMenu                            = "Главное меню" // TODO: Нормальный текст главного меню
-	Support                             = "Поддержечка"  // TODO: Нормальный текст поддержечки
-	NewOrderCreated                     = "Успешно создан новый заказ! \n%s"
+	StartMsg                              string = "Добро пожаловать в сервис ITMO.DELIVERY"
+	ErrorMsg                              string = "Упс... Что-то пошло не так"
+	UnknownAction                         string = "Неизвестное действие!"
+	SelectBuilding                        string = "Выберите нужное здание в меню."
+	IncorrectBuilding                            = "Неверное название здания! Попробуйте ещё раз."
+	SuccessfullySelectedBuildingFormatted        = "Успешно выбрано здание: %s"
+	SuccessfullyDescriptionInputFormatted        = "Успешно введено описание: %s"
+	MyOrders                              string = "Мои последние заказы"
+	ActiveOrders                          string = "Активные заказы:"
+	OrderConfirmMessageFormatted          string = "Вы подтверждаете заказ? \n%s"
+	InputDescription                      string = "Введите описание заказа:"
+	IncorrectDescriptionSizeFormatted            = "Некорректный размер сообщения! Допустимо не больше %d символов!"
+	Instruction                           string = "Инструкция"   // TODO: Нормальный текст инструкции
+	Feedback                              string = "Фидбек"       // TODO: Нормальный текст фидбека
+	AboutBot                              string = "О боте"       // TODO: Нормальный текст о боте
+	MainMenu                                     = "Главное меню" // TODO: Нормальный текст главного меню
+	Support                                      = "Поддержечка"  // TODO: Нормальный текст поддержечки
+	NewOrderCreated                              = "Успешно создан новый заказ! \n%s"
+	BackButtonClicked                            = "Переходим назад..."
 )
 
 var StateMessage = map[model.UserState]string{
